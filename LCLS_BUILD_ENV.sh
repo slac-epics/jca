88c43ea72c1627348763a138c821d099b04ac765
Hugo Slepicka <slepicka@slac.stanford.edu>
2018-09-05 16:11:25 -0700

    Removed hardcoded paths. Using Java as jdk1.8.0_181 Using Ant as apache-ant-1.7.0

--- LCLS_BUILD_ENV.sh
+++ LCLS_BUILD_ENV.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
-source /afs/slac/g/lcls/epics/setup/go_epics_3.15.5-1.0.bash
-export JAVA_HOME=/afs/slac/g/lcls/package/java/jdk1.8.0_144
-export PATH=$JAVA_HOME/bin:$PATH
-export LD_LIBRARY_PATH=$JAVA_HOME/jre/lib/amd64:$LD_LIBRARY_PATH
+#source /afs/slac/g/lcls/epics/setup/go_epics_3.15.5-1.0.bash
+export JAVA_HOME=$PACKAGE_TOP/java/jdk1.8.0_181
+export ANT_HOME=$PACKAGE_TOP/ant/apache-ant-1.7.0
+export PATH=$JAVA_HOME/bin:$ANT_HOME/bin:$PATH
+export LD_LIBRARY_PATH=$JAVA_HOME/jre/lib/amd64:$ANT_HOME/lib:$LD_LIBRARY_PATH
 
 # Build with ant
-% ant
-....
+#% ant
+#....
 
 # Install with ant
-% ant install
-...
+#% ant install
+#...
